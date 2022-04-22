84567689977e13fc1c7a3e355c598c90944d03bd
Scooter <dragon.aka.scooter@gmail.com>
2022-04-22 22:50:48 +0900

    done

--- thinking.rb
+++ thinking.rb
@@ -1,7 +1,23 @@
-A0345
-B0123
-C0134
+list = []
 
-9C3 * 5
+['A', 'B', 'C', 'D', 'E'].each do |letter|
+  (1..9).to_a.each do |first|
+    code = "#{letter}0"
+    next if first > 7
+    code += first.to_s
+    code_temp1 = code
+    (first+1..9).to_a.each do |second|
+      code = code_temp1
+      next if second > 8
+      code += second.to_s
+      code_temp2 = code
+      (second+1..9).to_a.each do |third|
+        code = code_temp2
+        code += third.to_s
+        list << code
+      end
+    end
+  end
+end
 
-(9*8*7)/6 * 5
+
